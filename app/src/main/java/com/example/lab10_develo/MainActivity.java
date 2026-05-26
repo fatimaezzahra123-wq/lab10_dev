de471d7121baa80a73298b19900b3a1232731682
fatimaezzahra <f.ennassiri6175@uca.ac.ma>
2026-05-26 23:59:55 +0100

    first commit

---
+++ app/src/main/java/com/example/lab10_develo/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.lab10_develo;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_fragment1) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.contenu, new BlankFragment())
+                    .commit();
+        } else if (id == R.id.nav_fragment2) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.contenu, new BlankFragment2())
+                    .commit();
+        } else if (id == R.id.nav_list) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.contenu, new FragmentList())
+                    .commit();
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
